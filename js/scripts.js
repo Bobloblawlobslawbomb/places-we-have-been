df6224e647a8fc915e6e2738721a83c17a4cd43c
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-05 13:28:03 -0700

    begin the project with a destination contstructor
    Co-authored-by: Rami Ahmed<ramiahmedportland@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Need an opject
+//places
+
+
+
+//Destination contructor
+function Destination(location, landmarks, timeOfYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+
+/*
+Destinations.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+}
+*/
+
+
+let Destination
